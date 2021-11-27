76a60e9fb430e6d364d0bb1ccbca7411ce164fa0
Jeffrey <jeffrey.rodriigues@gmail.com>
2021-11-27 20:06:27 -0300

    Update Procject

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+  return(
+    <>
+    <h1 className="titulo">Home</h1>
+    <img src="https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y29mZmVlJTIwY3VwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Imagem Tela Inicial" className="img"/>
+    </>
+  );
+}
+
+export default Home;
